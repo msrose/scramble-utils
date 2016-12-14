8faa246286f1cac48d9799f09627cc24fc858f4c
Michael Rose <michael_rose@outlook.com>
2016-12-13 21:19:38 -0800

    Add basic scramble generating

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,32 @@
 const sg = require('../index');
 
 describe('Scramble generator', () => {
+  const faceList = ['RIGHT', 'UP', 'LEFT', 'DOWN', 'FRONT', 'BACK'];
+  const shortFaceList = faceList.map(m => m[0]);
+
+  it('exports the cube moves', () => {
+    const {
+      R, U, L, D, F, B,
+      RIGHT, UP, LEFT, DOWN, FRONT, BACK
+    } = sg.Faces;
+    expect([R, U, L, D, F, B]).toEqual(faceList);
+    expect([RIGHT, UP, LEFT, DOWN, FRONT, BACK]).toEqual(faceList);
+  });
+
   it('generates a scramble for 3x3x3', () => {
-    expect(sg('3x3x3').split(' ').length).toBe(20);
-    expect(sg('3x3x3').replace(/ /g, '')).toMatch(/[RUFDLB']{20,40}/);
+    const scramble = sg.generate('3x3x3');
+    expect(scramble.length).toBe(20);
+    scramble.forEach((move) => {
+      expect(typeof move.inverted).toBe('boolean');
+      expect(shortFaceList).toContain(move.face);
+    });
+  });
+
+  it('generates a formatted scramble for 3x3x3', () => {
+    expect(sg.formatted('3x3x3').replace(/[' ]/g, '')).toMatch(/[RULDFB]{20}/);
+  });
+
+  it('generates different 3x3x3 scrambles', () => {
+    expect(sg.formatted('3x3x3')).not.toBe(sg.formatted('3x3x3'));
   });
 });
--- index.js
+++ index.js
@@ -1,9 +1,49 @@
+const Faces = {
+  RIGHT: 'RIGHT',
+  LEFT: 'LEFT',
+  UP: 'UP',
+  DOWN: 'DOWN',
+  FRONT: 'FRONT',
+  BACK: 'BACK'
+};
+
+const faceArray = Object.keys(Faces);
+
+for(const face in Faces) {
+  Faces[face[0]] = Faces[face];
+}
+
+const randomInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
 const generators = {
   '3x3x3'() {
-    return 'R U R U R U R U R U R U R U R U R U R U';
+    const scramble = [];
+    for(let i = 0; i < 20; i++) {
+      const face = faceArray[randomInRange(0, faceArray.length)];
+      scramble.push({
+        inverted: randomInRange(0, 2) === 0,
+        face: face[0],
+        longFace: face
+      });
+    }
+    return scramble;
   }
 };
 
-module.exports = (puzzle) => {
+const generate = (puzzle) => {
   return generators[puzzle]();
+};
+
+const formatted = (puzzle) => {
+  return generate(puzzle).map(move =>
+    `${move.face}${move.inverted ? "'" : ''}`
+  ).join(' ');
+};
+
+module.exports = {
+  Faces,
+  generate,
+  formatted
 };
